b331588e936dfdd4e3eb2e28899930952c1c740d
Roy <luoyin2018@163.com>
2023-03-07 20:21:15 +0800

    feat[ParticleForceInterface]: Add basic framework for applying force for particle

---
+++ CyclonePhysicsBook/ParticleForceRegistry.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cyclone
+{
+    public interface IParticleForceGenerator
+    {
+        void UpdateForce(Particle particle, float duration);
+    }
+
+    public class ParticleForceRegistry
+    {
+        List<(IParticleForceGenerator, Particle)> registration
+            = new List<(IParticleForceGenerator, Particle)>();
+        public IReadOnlyCollection<(IParticleForceGenerator,Particle)> Registration 
+            => new ReadOnlyCollection<(IParticleForceGenerator, Particle)>(registration);
+
+        public void Add(IParticleForceGenerator forceGenerator, Particle paricle)
+        {
+            registration.Add((forceGenerator, paricle));
+        }
+        public void Remove(IParticleForceGenerator forceGenerator, Particle particle)
+        {
+            var items = registration.Where(r => r.Item1 == forceGenerator && r.Item2 == particle);
+            foreach (var item in items)
+            {
+                registration.Remove(item);
+            }
+        }
+        public void Clear()
+        {
+            registration.Clear();
+        }
+
+        public void Update(float duration)
+        {
+            foreach(var (forcegenerator, paricle) in registration)
+            {
+                forcegenerator.UpdateForce(paricle, duration);
+            }
+        }
+    }
+}
